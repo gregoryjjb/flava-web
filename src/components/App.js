727cdb1906dbffb0483b56e725e01903c72ebf34
Greg Brisebois <gregoryjjb@gmail.com>
2018-09-12 17:14:38 -0700

    Basic MUI home test page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-const App = ({ message }) => (
-    <div>
-        <h1>Welcome to Flavatown</h1>
-        <h2>Hello there... {message}</h2>
+import { withStyles, Typography } from "@material-ui/core";
+
+const styles = theme => ({
+    root: {
+        background: theme.palette.primary.light,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        padding: 16,
+    },
+    text: {
+        color: theme.palette.getContrastText(theme.palette.primary.light),
+    },
+});
+
+const App = ({ classes, message }) => (
+    <div className={classes.root}>
+        <Typography variant="display2" gutterBottom className={classes.text}>
+            Welcome to Flavatown
+        </Typography>
+        <Typography variant="title">Hello there... {message}</Typography>
     </div>
 );
 
-export default App;
+export default withStyles(styles)(App);
